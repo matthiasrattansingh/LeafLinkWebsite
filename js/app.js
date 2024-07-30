292aaf338322e43f6f5beda9b8554ec93f2a69fd
Josh-Thomas9784 <114836831+Josh-Thomas9784@users.noreply.github.com>
2024-07-30 14:23:45 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,19 +1,5 @@
 
 
-// navbar scroll
-var lastScrollTop = 0;
-navbar = document.getElementById('header');
-window.addEventListener("scroll", function(){
-  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  if (scrollTop > lastScrollTop){
-    navbar.style.top="-80px";
-  } else{
-    navbar.style.top="0px";
-  }
-  lastScrollTop = scrollTop;
-})
-
-
 // DARK MODE
 // check for saved 'darkMode' in localStorage
 let darkMode = localStorage.getItem('darkMode'); 
